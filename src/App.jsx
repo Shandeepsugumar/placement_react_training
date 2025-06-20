14d0c743b7e1bf2faccb1a3a64dd86cf6d6968c2
Shandeepsugumar <shandeepgeek@gmail.com>
2025-06-20 16:11:48 +0530

    react second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 import Welcome from './welcome'
+import Skills from './skills'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Services from './Pages/Services'
+import About from './Pages/About'
+import Navbar from './components/Navbar'
+import State from './hooks/State'
+import Form from './hooks/Form'
+import Effect from './hooks/Effect'
 
 function App() {
 
   return (
     <>
-      <Welcome name='shandeep' country='India'/>
-      <h1>Hello World</h1>
-      <Skills skill={['React','Node','Express','MongoDb']}/> 
+      <Navbar/>
+      <Routes>
+        <Route path ='/' element={<Home/>}/>
+        <Route path='/About' element={<About/>}/>
+        <Route path='/Services' element={<Services/>}/>
+        <Route path='/Welcome' element={<Welcome name='shandeep' country='India'/>}/>
+        <Route path='/Skills' element={<Skills skill={['React','Node','Express','MongoDb']}/>}/>
+        <Route path='/state' element={<State/>}/>
+        <Route path='/form' element={<Form/>}/>
+        <Route path='/effect' element={<Effect/>}/>
+      </Routes>
+      <h1>Hello World</h1> 
     </>
   )
 }
---
+++ src/Pages/Services.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+const Services = () => {
+    return (
+        <>
+            <h1>Services</h1>
+        </>
+    )
+}
+export default Services
